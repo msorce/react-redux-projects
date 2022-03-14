439d2f449d4360061e809373cfafa4a99b29c452
msorce <marksorce@gmail.com>
2022-03-13 23:15:52 -0700

    pics app complete, dynamic grid row ends

--- pics/src/components/ImageList.js
+++ pics/src/components/ImageList.js
@@ -1,11 +1,12 @@
 import React from "react";
-
+import "./ImageList.css"
+import ImageCard from "./ImageCard";
 const ImageList = (props) => {
-    const images = props.images.map(({ id, description, urls }) => (
-        <img key={id} alt={description} src={urls.regular} />
+    const images = props.images.map((image) => (
+        <ImageCard image={image} />
     ));
     console.log(props.images);
-    return <div>{images}</div>;
+    return <div className="image-list" >{images}</div>;
 };
 
 export default ImageList;
